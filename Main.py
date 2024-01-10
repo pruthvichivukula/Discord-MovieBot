4f60cf4a75d5cd0aad3de49e16892506cd0ff147
pruthvichivukula <pruthvichivukula@cmail.carleton.ca>
2024-01-09 19:01:02 -0500

    adding boilerplate discord bot code

--- Main.py
+++ Main.py
@@ -1,1 +1,27 @@
-print("Hello World")
+import Credentials.credentials_discord as cred_d
+import discord
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+    if message.content.startswith('!test'):
+        await message.channel.send("TestReceived")
+
+client.run(cred_d.disc_token)
+
+
