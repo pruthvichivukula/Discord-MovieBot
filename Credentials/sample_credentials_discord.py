b3da477abd494b24f6a8bd514349193b964a9620
pruthvichivukula <pruthvichivukula@cmail.carleton.ca>
2024-01-09 19:00:42 -0500

    adding a sample cred file

---
+++ Credentials/sample_credentials_discord.py
@@ -0,0 +1,2 @@
+# Make a file called "credentials_disord.py" and make a variable like the one below. replace the string with the token for your bot
+disc_token = "abcdefghijklmnopqrstuvwxyz.qwertyuiopasdfghjklzxcvbnm"
